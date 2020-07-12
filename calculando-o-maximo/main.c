68cff880d600175fbf6252b719f0e56d2f033f0f
Fabio Flaitt Junior <CIANDT\fabiofj@lnb024645cps.ciandt.global>
2020-07-12 17:31:46 -0300

    Calculando o maximo valor de um vetor

---
+++ calculando-o-maximo/main.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+// [1,2]
+int maximo(int *v, int tam, int novo_max){
+    if(tam == 1) {
+        if (v[tam-1]>novo_max) {
+            return v[tam-1];
+        } else {
+            return novo_max;
+        }
+    }
+    if (novo_max > v[tam-1]) {
+        return maximo(v, tam-1, novo_max);        
+    } else {
+        return maximo(v, tam-1, v[tam-1]);
+    }
+}
+
+int main (){
+    int vetor[10] = {1,2,3,4,15,6,7,8,9,10};
+    int tamanho_do_vetor = 10;
+
+    printf("O maximo eh: %i \n", maximo(vetor,tamanho_do_vetor, -1));
+    return 0;
+}
